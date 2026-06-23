3df3a0f6b7d5768f18f09e5e0a8b19f995db8022
FilipeGuidi <filipe.guidi@gmail.com>
2026-06-23 00:47:06 +0000

    Adicionando nova atividade em JS

---
+++ Atividade7/persistente.js
@@ -0,0 +1,12 @@
+function salvarNome(){
+    let valorDigitado = document.getElementById("texto-inserido").value;
+    let objetoNome = {
+        nome: valorDigitado
+    };
+    let textoJSON = JSON.stringify(objetoNome)
+    localStorage.setItem("dadoNome", textoJSON)
+
+    document.getElementById("nome-salvo").innerText = valorDigitado;
+
+
+}
