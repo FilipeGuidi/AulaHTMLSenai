e770106e25eae9136b9086e7b7a493742cb159aa
FilipeGuidi <filipe.guidi@gmail.com>
2026-06-24 00:49:21 +0000

    Adicionando JS a atividade 8

---
+++ Atividade8/js/Funcoes.js
@@ -0,0 +1,18 @@
+function soma(){
+     let primeiroNumero = document.getElementById("primeiro-numero").value;
+    let segundoNumero = document.getElementById("segundo-numero").value;
+
+    let resultado = Number(primeiroNumero) + Number(segundoNumero);
+
+    document.getElementById("resultado").innerText = resultado
+    
+     salvarHistorico("Soma",primeiroNumero, segundoNumero, resultado)
+}
+
+
+function salvarHistorico(nomeDaFuncao, num1, num2, resultado) {
+    console.log("Função: " + nomeDaFuncao);
+    console.log("Primeiro numero: " + num1 + ", SegundoNumero: " + num2);
+    console.log("Resultado: " + resultado);
+    console.log("--------------------------------------");
+}
